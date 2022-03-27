21794fc6c3523e2240ba5f517a4d8107fc5c4636
Manning <farjad_uk@hotmail.com>
2022-03-27 17:38:58 -0400

    multiple/single products

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -7,10 +7,68 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  Product.findAll({
+    attributes: [
+      'id',
+      'product_name',
+      'price',
+      'stock',
+      'category_id'
+    ],
+    include: [
+      {
+        model: Category,
+        attributes: [
+        'id',
+        'category_name'
+      ],
+      },
+      {
+        model: Tag,
+        through: ProductTag,
+        as: 'tags',
+      },
+    ],
+  })
+    .then((dbProductData) => res.json(dbProductData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 // get one product
 router.get('/:id', (req, res) => {
+  Product.findOne({
+    where: {
+      id: req.params.id,
+    },
+    include: [
+      {
+        model: Category,
+        attributes: [
+          'id',
+          'category_name'
+      ],
+      },
+      {
+        model: Tag,
+        through: ProductTag,
+        as: 'tags',
+      },
+    ],
+  })
+    .then((dbProductData) => {
+      if (!dbProductData) {
+        res.status(404).json({ message: "There was no product found with this id." });
+        return;
+      }
+      res.json(dbProductData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
 });
