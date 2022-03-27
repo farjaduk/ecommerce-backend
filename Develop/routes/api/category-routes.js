76d18865b2956ab4f7e22c3e90225f03795e74f4
Manning <farjad_uk@hotmail.com>
2022-03-27 15:59:32 -0400

    find all categories

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -7,6 +7,29 @@ router.get('/', async (req, res) => {
   // find all categories
    
   // be sure to include its associated Products
+  Category.findAll({
+    attributes: [
+      'id',
+      'category_name'
+    ],
+    include: [
+      {
+        model: Product,
+        attributes: [
+          'id',
+          'product_name',
+          'price',
+          'stock',
+          'category_id'
+        ]
+      }
+    ]
+  })
+  .then(dbCategoryData => res.json(dbCategoryData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  })
 });
 
 router.get('/:id', (req, res) => {
